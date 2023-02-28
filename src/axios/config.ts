4b08b8cd686e9397ca6a0ae2b26d45fa3b0f0953
sonichka-s <stnchnk0@gmail.com>
2023-02-28 21:16:36 +0300

    adapted to safari

--- src/axios/config.ts
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-export const CHARACTER_URL = "/api/character/";
-
-export const $url = axios.create({
-  baseURL: "https://rickandmortyapi.com",
-});
--- src/utils/utils.ts
+++
@@ -1,7 +0,0 @@
-export const BREAKPOINT_XS = 320;
-export const BREAKPOINT_SM = 420;
-export const BREAKPOINT_MD = 768;
-export const BREAKPOINT_LG = 1000;
-
-export const CARDS_PER_PAGE_L = 4;
-export const CARDS_PER_PAGE_S = 2;
